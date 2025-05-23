4274dbfc9fba72d5e5c6aebe4a6aff92f54291f1
naif radwi <naif.mazen.radwi@gmail.com>
2025-05-23 18:51:24 +0300

    docs

--- lib/src/fetch_result_bloc.dart
+++ lib/src/fetch_result_bloc.dart
@@ -5,8 +5,31 @@ import 'package:fetch_result_bloc/src/fetch_result_event.dart';
 import 'package:fetch_result_bloc/src/fetch_result_state.dart';
 import 'package:result_flow/result_flow.dart';
 
+/// An abstract BLoC (Business Logic Component) for fetching data.
+///
+/// It handles common states like
+/// - initial
+/// - loading
+/// - refreshing
+/// - loaded
+/// - error.
+/// Extend this class to implement specific data fetching logic by
+/// overriding [getResult].
+///
+/// Type parameters:
+/// - [P]: The type of parameters required for fetching the data.
+/// - [T]: The type of the data to be fetched.
 abstract class FetchResultBloc<P, T>
     extends Bloc<FetchResultEvent<P>, FetchResultState<T>> {
+  /// Creates a [FetchResultBloc].
+  ///
+  /// - [initialState]: The initial state of the BLoC. Defaults to
+  ///   [FetchResultState.initial].
+  /// - [onLoadTransformer]: An optional [EventTransformer] for
+  ///   [FetchLoadResultEvent]. Can be used to customize event processing,
+  ///   e.g., using `bloc_concurrency` transformers.
+  /// - [onRefreshTransformer]: An optional [EventTransformer] for
+  ///   [FetchRefreshResultEvent]. Can be used to customize event processing.
   FetchResultBloc({
     FetchResultState<T>? initialState,
     this.onLoadTransformer,
@@ -19,9 +42,22 @@ abstract class FetchResultBloc<P, T>
     );
   }
 
+  /// An optional [EventTransformer] to customize the processing
+  /// of [FetchLoadResultEvent]s.
+  ///
+  /// For example, use `droppable()` or `restartable()` from `bloc_concurrency`.
   final EventTransformer<FetchLoadResultEvent<P>>? onLoadTransformer;
+
+  /// An optional [EventTransformer] to customize the processing
+  /// of [FetchRefreshResultEvent]s.
+  ///
+  /// For example, use `droppable()` or `restartable()` from `bloc_concurrency`.
   final EventTransformer<FetchRefreshResultEvent<P>>? onRefreshTransformer;
 
+  /// Handles the [FetchLoadResultEvent] to initiate data fetching.
+  ///
+  /// Emits a [FetchResultState.loading] state before fetching.
+  /// If already loading or refreshing, this method does nothing.
   Future<void> _onLoad(
     FetchLoadResultEvent<P> event,
     Emitter<FetchResultState<T>> emit,
@@ -32,6 +68,10 @@ abstract class FetchResultBloc<P, T>
     return _fetchResult(params, emit);
   }
 
+  /// Handles the [FetchRefreshResultEvent] to initiate data refreshing.
+  ///
+  /// Emits a [FetchResultState.refreshing] state before fetching.
+  /// If already loading or refreshing, this method does nothing.
   Future<void> _onRefresh(
     FetchRefreshResultEvent<P> event,
     Emitter<FetchResultState<T>> emit,
@@ -42,8 +82,19 @@ abstract class FetchResultBloc<P, T>
     return _fetchResult(params, emit);
   }
 
+  /// Abstract method to be implemented by subclasses to perform
+  /// the actual data fetching.
+  ///
+  /// - [params]: The parameters required for fetching data.
+  /// Returns a [FutureResult] containing either the fetched data [T] or
+  /// a [ResultError].
   FutureResult<T> getResult(P params);
 
+  /// Common logic to fetch data using [getResult] and emit corresponding
+  /// states.
+  ///
+  /// - [params]: The parameters for [getResult].
+  /// - [emit]: The emitter to output new states.
   Future<void> _fetchResult(P params, Emitter<FetchResultState<T>> emit) async {
     final result = await getResult(params);
     result.on(
